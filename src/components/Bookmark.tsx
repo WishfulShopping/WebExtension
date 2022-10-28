3b3c6fad515c04af5a9c5d19d04ced5f2ef1cbac
Pascal Noisette <netpascal0123@aol.com>
2022-10-28 10:56:13 +0200

    POC : send item to wishlist ok

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import type Types from "src/types";
 
-export const Bookmark = (props: { bookmark: Types.Bookmark }) => {
+export const Bookmark = (props: { bookmark: Types.Bookmark, origin: string }) => {
+  const [response, setResponse] = useState<any>();
+  const bookmark = props.bookmark;
+
+  useEffect(() => {
+    const body = new FormData();
+    body.append('url', bookmark.url);
+    body.append('html', bookmark.html);
+    bookmark.images.forEach(e => { return body.append('images', e) });
+
+    fetch(props.origin, {
+        method: 'POST',
+        body
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json()
+    }).then((response) => {
+      response.status = "Done";
+      setResponse(response)
+     }).catch((e) => {
+       setResponse({ title: e.message, status: "Error" })
+    })
+  }, [bookmark, props.origin]);
+  
+  if (!response) {
     return (
       <div>
-        Adding {props.bookmark.url} to the wishlist
+        Adding {bookmark.url} to the wishlist
       </div>
     );
+  }
+  
+  return <div>{ response.status }<p>{ response.title }</p></div>;
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,8 @@ import type Types from "src/types";
 export const Button: React.VFC = () => {
   const [currentTab, setCurrentTab] = useState<number>();
   const [bookmark, setBookmark] = useState<Types.Bookmark>();
+  const [isGranted, setIsGranted] = useState<boolean>(false);
+  const origin = 'https://wishlist.netpascal.site/api/url/add/cc2d98ec63dde1c2cb805a4a647cc53b';
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -23,16 +25,35 @@ export const Button: React.VFC = () => {
     chrome.tabs.sendMessage<Types.Message>(currentTab, { action: "getId" }, (res: Types.Response) => {
         setBookmark(res.data);
     });
+    
+    const perm = {
+      permissions: ['tabs'],
+      origins: [origin]
+    };
+
+    chrome.permissions.contains(perm, (result) => {
+      if (result) {
+        // The extension has the permissions.
+        setIsGranted(true);
+      } else {
+        chrome.permissions.request(perm, (granted) => {
+          // The callback argument will be true if the user granted the permissions.
+          if (granted) {
+            setIsGranted(true);
+          }
+        });
+      }
+    });
   };
 
   if (!currentTab) return <div>Loading...</div>;
-  if (!bookmark) return (
+  if (!bookmark || !isGranted) return (
     <button onClick={handleClick} className="block p-2 mx-auto rounded border">
       Click Me!
     </button>
   );
 
   return (
-    <Bookmark bookmark={bookmark}/>
+    <Bookmark bookmark={bookmark} origin={origin} />
   );
 };
