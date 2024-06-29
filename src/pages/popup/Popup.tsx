9b17f34f98f83910b602d21c4751ad3e86a7e83a
Pascal Noisette <netpascal0123@aol.com>
2024-06-29 22:18:36 +0200

    Nice popin to link to option page and add to wishlist

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,25 +1,54 @@
 import React from "react";
-
-import logo from "@assets/img/logo.svg";
+import { Settings, Visibility } from '@mui/icons-material';
+import { Wishlist } from "./types";
 
 export default function Popup(): JSX.Element {
+  const [wishlists, setWishlists] = React.useState<Wishlist[]>([]);
+
+  React.useEffect(() => {
+    const defaultWishlist = { name: "Default", need_authentification: false, url: "", username: "", password: "" };
+     setWishlists([defaultWishlist]);
+  }, []);
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+    <div className="flex font-sans">
+      <div className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <h1 className="flex-auto text-lg font-semibold text-slate-900">
+            Wishful Shopping
+          </h1>
+          <div className="text-lg font-semibold text-slate-500">
+            <a
+              href="/options/index.html"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View options"
+              style={{ float: "right" }}
+            >
+              <Settings className="hover:text-gray-600" />
+            </a>
+          </div>
+          <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2 p-1">
+            <span>Bookmark this page into a wishlist</span>
+          </div>
+        </div>
+        {wishlists.map(wishlist => <>
+          <div className="flex space-x-1 mb-6 text-sm font-medium">
+            <button className="hover:bg-gray-600 bg-gray-500 w-52 h-16 font-semibold rounded-md text-white"  title="Add this page as wish">
+              Add to default wishlist
+            </button>
+            <div className="flex-col">
+              <a
+                href={`/options/index.html#/${wishlist.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="View wishlist content"
+              >
+                <Visibility className="hover:text-gray-600" />
+              </a>
+            </div>
+          </div>
+        </>)}
+      </div>
     </div>
   );
 }
---
+++ src/pages/popup/types.ts
@@ -0,0 +1,9 @@
+
+export type Wishlist = { 
+    id?: string,
+    name: string,
+    need_authentification: boolean,
+    url: string,
+    username: string,
+    password: string,
+};
